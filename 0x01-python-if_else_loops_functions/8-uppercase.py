ec723bf95a762e31267ec6944e27b209cc7d8800
Edwardmurithi <murithiedward330@gmail.com>
2024-03-12 08:28:19 -0700

    added 8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-# function that prints a string in uppercase followed by a new line.
+# prints a string in uppercase followed by a new line.
 def uppercase(str):
     for char in str:
-        ascii_value = ord(char)
-        if 97 <= ascii_value <= 122:  # check if the character is lowercase
-            ascii_value -= 32  # convert to uppercase by substracting 32
-        print(chr(ascii_value), end="")
-    print()  # print new line after printing uppercase string
+        if ord(char) >= 97 and ord(char) <= 122:
+            c = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")

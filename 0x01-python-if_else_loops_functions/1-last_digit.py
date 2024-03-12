f5498ed2f0269dd3c6ddf1a1d71f78d198e2dd1c
Edwardmurithi <murithiedward330@gmail.com>
2024-03-12 05:23:48 -0700

    modified error in 1-last_digit file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-cast_str = str(number)
-x = cast_str[-1]
-cast_int = int(x)
-if cast_int > 5:
-    print(f"Last digit of {number:d} is {x:s} and is greater than 5")
-elif cast_int == 0:
-    print(f"Last digit of {number:d} is {x:s} and is 0")
+
+last_digit = abs(number) % 10
+
+if last_digit > 5:
+    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number:d} is {last_digit:d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {x:s} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")

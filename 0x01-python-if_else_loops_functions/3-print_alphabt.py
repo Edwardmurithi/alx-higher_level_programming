3ad8b61be8ea23f64d1fb98dc5491cd03b540484
Edwardmurithi <murithiedward330@gmail.com>
2024-03-12 06:35:45 -0700

    added 3-print_alphabt

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Edward murithi
+#<murithiedward330@gmail.com>
+
+"""Print all the letters except q and e and not followed by new line"""
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")

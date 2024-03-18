f17c1f796737ed6f2a3856a27d5472bc664433c4
Edwardmurithi <murithiedward330@gmail.com>
2024-03-18 00:12:14 -0700

    created a funtion to print items in a list each in it line

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Function that prints all integers of a list
+    """
+    for integer in my_list:
+        print('{}'.format(integer))

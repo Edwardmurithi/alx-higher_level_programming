f442d35f807d0f83ad11ad4232ba4aa6131eb76e
Edwardmurithi <murithiedward330@gmail.com>
2024-03-18 02:04:14 -0700

    added a  function that replaces an element of a list at a specific position (like in C)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/pyhton3
 
 def replace_in_list(my_list, idx, element):
-    """function that replaces an element of a list at a specific position (like in C)
+    """function that replaces an element of a list at a
+    specific position (like in C)
     """
     for i in range(len(my_list)):
         if idx == i:

55dc2d9aeac8997ed394d83dda30684fa738b730
Edwardmurithi <murithiedward330@gmail.com>
2024-06-17 02:44:00 -0700

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-let print_lines = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing';
-console.log(print_lines);
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');

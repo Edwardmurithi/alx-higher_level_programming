04da2c032be6521e899f3e9e95a858400cadeed8
Edward murithi <murithiedward330@gmail.com>
2024-06-17 15:26:27 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-let number_args = process.argv.length - 2;
+const count = process.argv.length - 2;
 
-if (number_args === 0) {
+if (count === 0) {
 	console.log("No argument");
-} else if(number_args === 1) {
+} else if(count === 1) {
 	console.log("Argument found");
 } else {
 	console.log("Arguments found");

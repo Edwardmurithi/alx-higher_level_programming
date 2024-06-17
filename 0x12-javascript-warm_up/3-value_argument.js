2e32235d12a00ea37400fd0555c134ab3ece8528
Edward murithi <murithiedward330@gmail.com>
2024-06-17 16:46:59 +0300

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const arguments = process.argv;
-// check if argument exists and printing it
-if (process.argv[2] !== undefined) {
-	console.log(process.argv[2]);
-} else {
-	console.log('No argument');
-}
+console.log(process.argv[2] !== undefined ? process.argv[2] : 'No argument');

023873db8ff935b25c86f3c25e2b2d643eb5f7b0
Edward murithi <murithiedward330@gmail.com>
2024-06-18 11:19:24 +0300

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const count = process.argv;
-if (process.argv[2] !== undefined) {
-	console.log(process.argv[2]);
+const count = process.argv
+if (typeof process.argv[2] === 'undefined') {
+	console.log('No argument');
 } else {
-	console.log("No argument");
+	console.log(process.argv[2]);
 }
+
